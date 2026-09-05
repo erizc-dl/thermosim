71c41afacab07113bd95322d92d6f5fcf5813393
Eric Zhang <246936143+erizc-dl@users.noreply.github.com>
2026-09-05 17:45:04 -0400

    Added drift rate and temperature loss

--- thermosim.py
+++ thermosim.py
@@ -1,12 +1,14 @@
 """Thermosim - Thermostat Simulation Program"""
 
 # unit - celcius
-indoor_temp = 20
+indoor_temp = 30
+outdoor_temp = 40
 
 cool_rate = -0.2
 heat_rate = 0.2
+drift_rate = 0.05
 
-target_temp = 120
+target_temp = 25
 
 sim_time = 1440 # set sim time (min)
 
@@ -21,22 +23,32 @@ def heater(temp):
     temp += heat_rate
     return temp
 
+def drift(temp):
+    if temp > outdoor_temp:
+        temp -= drift_rate
+        return temp
+    elif temp < outdoor_temp:
+        temp += drift_rate
+        return temp
+    else:
+        return temp # no drift
+
 def thermostat(temp, target):
     if temp > target:
         return airConditioner(temp)
     elif temp < target:
         return heater(temp)
     else:
-        return None
+        return temp
 
 for i in range(sim_time):
     print(f"{hour}:{min} - {indoor_temp} °C")
 
-    if thermostat(indoor_temp, target_temp) is None:
+    if thermostat(indoor_temp, target_temp) == indoor_temp:
         print(f"{hour}:{min} - *thermostat off")
-        break
 
     new_temp = thermostat(indoor_temp, target_temp)
+    new_temp = drift(new_temp)
     indoor_temp = round(new_temp, 2)
 
     min += 1
